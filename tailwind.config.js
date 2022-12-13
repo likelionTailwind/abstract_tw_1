d1b8acee15ebf3a6ea8717dd717170b42a7255a1
Juhee-Hwang <julia0276@naver.com>
2022-12-13 21:10:25 +0900

    [init] 초기세팅

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html'],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'blue': '#4C5FD5',
+      'lilac': '#dadbf1',
+    },
+  },
+  plugins: [],
+}

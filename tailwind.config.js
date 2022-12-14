3a4d29a6d19bc360671b5b4f705ec5b4b47d7062
jaesukpark77 <jaesukpark77@naver.com>
2022-12-14 20:58:23 +0900

    Build: 최초 빌드 파일 수정
    font-family 추가/module.export 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   content: ['./src/**/*.html'],
   theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'black': '#000000',
-      'blue': '#4C5FD5',
-      'lilac': '#dadbf1',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'black': '#000000',
+        'blue': '#4C5FD5',
+        'lilac': '#dadbf1',
+      },
     },
   },
   plugins: [],

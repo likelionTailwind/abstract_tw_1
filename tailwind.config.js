9c4fd26a246811aea4d8d51f8810f46d9114538f
chaehaeun <chae_3210@naver.com>
2022-12-15 20:34:34 +0900

    Build: bg-img 및 width 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html'],
+  content: ["./src/**/*.html"],
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'white': '#ffffff',
-        'black': '#000000',
-        'blue': '#4C5FD5',
-        'lilac': '#dadbf1',
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        black: "#000000",
+        blue: "#4C5FD5",
+        lilac: "#dadbf1",
+      },
+      backgroundImage: {
+        branch: "url('../assets/branch.png')",
+        human: "url('../assets/human.png')",
+        puzzle: "url('../assets/puzzle.png')",
+        dollar: "url('../assets/dollar.png')",
+        key: "url('../assets/key.png')",
+        message: "url('../assets/message.png')",
+      },
+      width: {
+        "txt-box": "calc(100% - 5rem)",
+      },
+      maxWidth: {
+        "screen-3xl": "1600px",
       },
     },
   },
   plugins: [],
-}
+};
